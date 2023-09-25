acdf835b783e29a9ac21d2ecfbd162b048e6927e
StoichoManolov <badmonsterxd@gmail.com>
2023-09-25 08:28:59 +0300

    Homework

--- Python - Fundamentals/6.Data Types and Variables/Lab/6. Next Happy Year.py
+++ Python - Fundamentals/6.Data Types and Variables/Lab/6. Next Happy Year.py
@@ -4,7 +4,7 @@ while True:
 
     year += 1
     year_str = str(year)
-# set removes same numbers eg: 9122(len 4) -> 912 (len 3)
+# set removes same numbers eg: 9122(len 4) -> 912 (len 3) / works for letters as well
     if len(set(year_str)) == len(year_str):
         break
 print(year)
