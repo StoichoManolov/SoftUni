386392a0565e1e615b66c588234d59ba138b6e43
StoichoManolov <badmonsterxd@gmail.com>
2024-03-05 13:41:19 +0200

    oop

---
+++ Python - OOP/Inheritance/ex_2_zoo/project/bear.py
@@ -0,0 +1,9 @@
+from project.mammal import Mammal
+
+
+class Bear(Mammal):
+    pass
+
+# mammal = Mammal("Stella")
+# print(mammal.__class__.__bases__[0].__name__)
+# print(mammal.name)
---
+++ Python - OOP/Inheritance/ex_2_zoo/project/main.py
@@ -0,0 +1,5 @@
+from project import reptile, snake
+from project.animal import Animal
+
+
+Animal
